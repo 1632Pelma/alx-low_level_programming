0628187cf689a8b57efbfa2df819c3b92a24b793
1632Pelma <pelmamonere@gmail.com>
2023-03-20 12:43:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,11 @@
 
 
 /**
- *
  * main - A program that prints using puts function
  * return 0.
- *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
